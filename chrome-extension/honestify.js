f5ecf9e3c9c1b34d830295c32370c0aecc614c1e
Bryan Stedman <bryanbstedman@gmail.com>
2013-02-18 16:16:30 -0800

    Initial Commit

---
+++ chrome-extension/honestify.js
@@ -0,0 +1,21 @@
+$.getJSON('https://raw.github.com/bryanstedman/honestify/master/phrases.json', function(data) {
+  var phrases = data;
+
+  function htmlreplace(a, element) {    
+    if (!element) element = document.body;    
+    var nodes = element.childNodes;
+    for (var n=0; n<nodes.length; n++) {
+      if (nodes[n].nodeType == Node.TEXT_NODE) {
+        for (key in a) {
+          var r = new RegExp(key, 'gi');
+          nodes[n].textContent = nodes[n].textContent.replace(r, a[key]);
+        }
+      } else {
+        htmlreplace(a, nodes[n]);
+      }
+    }
+  }
+
+htmlreplace(phrases);
+
+});
